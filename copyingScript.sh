6440906ba9dab0375c2eee9514ad2db66b629eb5
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2017-07-13 11:48:31 +0100

    basic working copying script, todo present inside

--- copyingScript.sh
+++ copyingScript.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO:
+# - help argument
+# - check format
+# - check if all arguments are present
+
 # Get the directoy to copy iterate and copy from
 dir_to_copy_from="$1"
 
@@ -9,14 +14,11 @@ dir_to_copy_to="$2"
 # Get the date to compare against the date accessed
 date_to_compare="$3"
 
-# Check formatting of directories
-# TODO
-
 # Convert the date passed in to UNIX timestamp
 converted_date_to_compare=$(date -j -f "%a %b %d %T %Z %Y" "$date_to_compare" "+%s")
 
 # Iterate the directory passed in
-for d in $dir_to_copy_from;
+for d in "$dir_to_copy_from"*;
 do
 
     echo -e "\n";
@@ -24,7 +26,7 @@ do
     echo -e "Checking $d ... \n";
 
     # Get date of folder in UNIX seconds
-    date_of_curr_folder=$(stat -f "%B" "$d")
+    date_of_curr_folder="$(stat -f "%B" "$d")"
 
     # Check if current folder creation date is after date passed in
     if [ ${date_of_curr_folder} -ge ${converted_date_to_compare} ] ; then
@@ -32,12 +34,13 @@ do
         echo -e "Copying $d and its contents... \n";
 
         # Is after, copy this folder to destination folder
-        cp -pPR -v $d $dir_to_copy_to
+        cp -pPR -v "$d" "$dir_to_copy_to"
 
     else
 
         # Not before skip
         echo -e "Do not copy, skipping... \n";
+        continue
 
     fi
 
